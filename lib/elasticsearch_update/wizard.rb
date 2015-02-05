6ff28e2d245d7c5ef0b70a04a390a05cdddf6aec
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-05 16:51:16 -0500

    Add a wizard class to ask questions to the user

---
+++ lib/elasticsearch_update/wizard.rb
@@ -0,0 +1,51 @@
+require 'highline/import'
+
+module ElasticsearchUpdate
+  # This class is in charge of retrieving and downloading data.
+  class Wizard
+
+    def extension
+      choose do |menu|
+        menu.prompt = 'Which type of upgrade are we doing?  '
+
+        menu.choice(:deb) { @choice = '.deb' }
+        menu.choice(:zip) { @choice = '.zip' }
+        menu.choice(:rpm) { @choice = '.rpm' }
+        menu.choice(:tar) { @choice = '.tar.gz' }
+      end
+
+      @choice
+    end
+
+    def version
+      ask('What version of Elasticsearch should we update to? (major.minor.path) ', String) { |q| q.validate = /\d\.\d\.\d/ }
+    end
+
+    def host
+      response = ask('What is your Elasticsearch hostname? (Default: localhost) ', String) { |q| q.validate = /^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$/ }
+    end
+
+    def port
+      response = ask('What is your Elasticsearch port? (Default: 9200) ', String) { |q| q.validate = /^\d*$/ }
+    end
+
+    def es_location_hash
+      {
+        host: host,
+        port: port
+      }
+    end
+
+    def sudo_password
+      ask('What password should be used while updating Elasticsearch?') { |q| q.echo = "x" }
+    end
+
+    def download_hash
+      {
+        base_url: 'download.elasticsearch.org',
+        version: version,
+        extension: extension
+      }
+    end
+  end
+end
