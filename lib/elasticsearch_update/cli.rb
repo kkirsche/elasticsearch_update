750132b02c18c465db18b8d09f7b1b976430cd33
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-05 16:51:28 -0500

    Add the main "driver" class to walk us through

---
+++ lib/elasticsearch_update/cli.rb
@@ -0,0 +1,26 @@
+require 'logger'
+
+module ElasticsearchUpdate
+  # This class is in charge of retrieving and downloading data.
+  class Cli
+    def self.start
+      @log = Logger.new(STDOUT)
+      @log.level = Logger::INFO
+
+      @log.debug('Logger created for CLI.')
+
+      wizard = ElasticsearchUpdate::Wizard.new
+
+      es_client = ElasticsearchUpdate::Elasticsearch.new(wizard.es_location_hash)
+      es_client.disable_cluster_routing_allocation
+      es_client.shutdown_local_node
+
+      downloader = ElasticsearchUpdate::Downloader.new(wizard.download_hash)
+
+      file = downloader.download_file
+      downloader.verify_update_file
+
+      puts file.path
+    end
+  end
+end
