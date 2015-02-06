3a08a252e3f12c8eee2d96fb2a326c72e77bc310
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-06 09:41:09 -0500

    Extract commands to classes

--- bin/update.rb
+++ bin/update.rb
@@ -2,17 +2,6 @@ require 'elasticsearch_update'
 
 ElasticsearchUpdate::Cli.start if __FILE__ == $PROGRAM_NAME
 
-  # log.info('Updating elasticsearch from .deb file.')
-  # command = 'echo ' + sudo_passwd + ' | sudo -S dpkg -i "' + update_file.path + '"'
-  # puts command
-  # command_result = system(command)
-
-  # log.info('Starting elasticsearch service')
-  # system('echo ' + sudo_passwd + ' | sudo -S service elasticsearch start')
-
-  # log.debug('Establishing a new elasticsearch client link')
-  # client = Elasticsearch::Client.new log: false
-
   # log.info('Re-enabling elasticsearch cluster routing allocation')
   # client.cluster.put_settings body:
   # { transient: { 'cluster.routing.allocation.enable' => 'all' } }
