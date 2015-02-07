c83ff11f14aa1298d04f332b6a643a9a37ecf41b
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-07 00:53:08 -0500

    Add enable cluster routing method

--- lib/elasticsearch_update/elasticsearch.rb
+++ lib/elasticsearch_update/elasticsearch.rb
@@ -33,6 +33,24 @@ module ElasticsearchUpdate
       response
     end
 
+    def enable_cluster_routing_allocation
+      @log.info('Enabling cluster routing allocation')
+
+      begin
+        req = Net::HTTP::Put.new('/_cluster/settings',
+                                 'Content-Type' => 'application/javascript')
+        req.body = { transient: { 'cluster.routing.allocation.enable' => 'all' } }
+        req.body = req.body.to_json
+        response = Net::HTTP.new(@es_host, @es_port).start {|http| http.request(req) }
+      rescue Errno::ECONNREFUSED
+        puts 'Connection could not be made to Elasticsearch at'
+        puts @es_host + ':' + @es_port + '/_cluster/settings'
+        abort('Please verify that Elasticsearch is available at this address.')
+      end
+
+      response
+    end
+
     def shutdown_local_node
       @log.info('Shutting down local node')
       @shutdown_uri = URI('http://' + @es_host + ':' + @es_port.to_s + '/_cluster/nodes/_local/_shutdown')
