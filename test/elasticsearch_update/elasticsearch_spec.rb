ec1caf404c21791282be34f8f2fd1a63871971f2
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-09 13:26:16 -0500

    Test for cluster commands

--- test/elasticsearch_update/elasticsearch_spec.rb
+++ test/elasticsearch_update/elasticsearch_spec.rb
@@ -12,6 +12,28 @@ module ElasticsearchUpdate
         @es_client.es_port.must_equal 9200
         @es_client.es_host.must_equal 'localhost'
       end
+
+      it 'should disable cluster allocation' do
+        @es_client = Elasticsearch.new({ host: 'localhost', port: 9200 }, true)
+
+        @mock_http = Minitest::Mock.new
+        @mock_http.expect(:start, 200)
+
+        Net::HTTP.stub :new, @mock_http do
+          @es_client.disable_cluster_routing_allocation.must_equal 200
+        end
+      end
+
+      it 'should enable cluster allocation' do
+        @es_client = Elasticsearch.new({ host: 'localhost', port: 9200 }, true)
+
+        @mock_http = Minitest::Mock.new
+        @mock_http.expect(:start, 200)
+
+        Net::HTTP.stub :new, @mock_http do
+          @es_client.enable_cluster_routing_allocation.must_equal 200
+        end
+      end
     end
   end
 end
