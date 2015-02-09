b8117d1b6557e2fdc503d17a4748e62523eaf5f4
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-09 12:31:44 -0500

    New test for verifying files

--- test/elasticsearch_update/downloader_spec.rb
+++ test/elasticsearch_update/downloader_spec.rb
@@ -45,6 +45,19 @@ module ElasticsearchUpdate
           @downloader.download_remote_sha1.must_equal 'd377e39343e5cc277104beee349e1578dc50f7f8'
         end
       end
+
+      it "should retrieve the verify the file's SHA1 value against Elasticsearch's" do
+        hash =
+        {
+          base_url: 'download.elasticsearch.org',
+          version: '1.4.2',
+          extension: '.deb'
+        }
+
+        @mock_result = StringIO.new 'd377e39343e5cc277104beee349e1578dc50f7f8  elasticsearch-1.4.2.deb'
+
+        Digest::SHA1.stub :file, @mock_result do
+          @downloader = TestDownloader.new(hash, true)
           @downloader.download_remote_sha1.must_equal 'd377e39343e5cc277104beee349e1578dc50f7f8'
         end
       end
