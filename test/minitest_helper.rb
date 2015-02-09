2d02f0db177dbf06d4dc2b61d4b43a3a2ca15807
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-09 14:49:05 -0500

    Fix how code coverage is reported

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,7 +1,8 @@
 require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require_relative('../bin/update.rb')
 require 'minitest/autorun'
-CodeClimate::TestReporter.start
+
 # ElasticsearchUpdate houses all tests for the
 # Elasticsearch Updater and all associated library files
 module ElasticsearchUpdate
