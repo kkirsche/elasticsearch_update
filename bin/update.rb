096d761f1ec185864105b2af407ac88066f6d98d
Kevin Kirsche <kkirsche@ansfederal.com>
2015-02-12 14:11:11 -0500

    Delete Gem

--- bin/update.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require 'elasticsearch_update'
-
-args = ARGV.dup
-ARGV.clear
-command = args.shift.strip rescue 'help'
-
-ElasticsearchUpdate::Cli.start
--- test/minitest_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-require 'minitest/autorun'
-require 'elasticsearch_update'
-
-# ElasticsearchUpdate houses all tests for the
-# Elasticsearch Updater and all associated library files
-module ElasticsearchUpdate
-end
